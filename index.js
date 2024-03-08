f1e739749da94300fa48aaf424308b54c887db84
gungunsaluja <gungunsaluja16@gmail.com>
2024-03-08 23:59:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+// let url = "https://localhost:8000/users"
+
+// mongoose.connect('mongodb://127.0.0.1:27017/test');
+main()
+.then(()=>
+{
+    console.log("Connection successful");
+}).catch((err) => console.log(err));
+
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+  
+}
+const userSchema = new mongoose.Schema({
+    name:String,
+    email:String,
+    age:Number,
+});
+
+const user2 = new User({
+    name:"Adamee",
+    email:"gungunsaleeeu16@gmail.com",
+    age:48,
+});
+user2.save().then((res)=>{
+    console.log(res);
+}).catch(err=>
+    console.log(err));
+
+const User = mongoose.model("User",userSchema);
+User.insertMany([
+    {name:"Gungun",email:"gungun@16gmail.com,age:50"},
+    {name:"Peter",email:"ggungun@16gmail.com,age:55"},
+    {name:"Guungun",email:"gunguniiiii@16gmail.com,age:90"},
+]).then((res)=>{
+    console.log(res);
+})
+
